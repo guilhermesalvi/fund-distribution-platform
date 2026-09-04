7c909fcee5e69af6e0b7b83ff004e8f41d3f4011
Guilherme Salvi <guisalvi@outlook.com>
2026-09-03 21:39:40 -0300

    feat: add problem details exception handling

--- src/DemandConsolidation/Program.cs
+++ src/DemandConsolidation/Program.cs
@@ -4,10 +4,12 @@ var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.AddServiceDefaults();
 builder.AddDefaultApiVersioning();
+builder.Services.AddProblemDetails();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.UseExceptionHandler();
 app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
